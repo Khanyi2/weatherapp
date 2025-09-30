979478718d67bc24f554f43713d85d5ffad11831
khsefalcpt024 <khsefalcpt024@student.wethinkcode.co.za>
2025-09-30 21:37:37 +0200

    updated js file to implement api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "d0o3t764a6d83cea9c0bfd4fda2bd33e";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`; //does not return json of city details.
+  axios.get(apiURL).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value); //call funtion once the city has been searched.
 }
 
 let searchFormElement = document.querySelector("#search-form");
