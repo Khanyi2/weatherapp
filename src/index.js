7750570fc276819312ee08459b3add04ac006f8c
khsefalcpt024 <khsefalcpt024@student.wethinkcode.co.za>
2025-10-04 20:58:12 +0200

    updated code

--- src/index.js
+++ src/index.js
@@ -17,7 +17,6 @@ function refreshWeather(response) {
 }
 
 function formatDate(date) {
-  // let date = date.getDay();
   let minutes = date.getMinutes();
   let hours = date.getHours();
   let days = [
